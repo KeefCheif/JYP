8430b2a00d7416f4c3d1eee6892d16b610a01624
Keef Cheif <Keefcheif.dev@gmail.com>
2022-06-27 23:56:31 -0600

    Initial Commit

---
+++ JYP/JYPApp.swift
@@ -0,0 +1,17 @@
+//
+//  JYPApp.swift
+//  JYP
+//
+//  Created by peter allgeier on 6/27/22.
+//
+
+import SwiftUI
+
+@main
+struct JYPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
